50e08826e0b9968cbf9bbe9d22b922be289875bb
Bluscream <Bluscream@users.noreply.github.com>
2025-07-26 06:05:33 +0200

    a

--- classes/__init__.py
+++ classes/__init__.py
@@ -6,7 +6,7 @@ from openrgb.utils import RGBColor
 from .OpenRGBController import OpenRGBController
 from .EffectOptions import EffectOptions
 from .Effect import Effect
-from .Colors import Colors, RAINBOW_COLORS, lerp_color
+from .Colors import Colors, RAINBOW_COLORS, lerp_color, parse_color
 from .EffectDiscovery import EffectDiscovery
 
-__all__ = ['OpenRGBController', 'EffectOptions', 'Effect', 'Colors', 'RAINBOW_COLORS', 'lerp_color', 'EffectDiscovery', 'RGBColor'] 
+__all__ = ['OpenRGBController', 'EffectOptions', 'Effect', 'Colors', 'RAINBOW_COLORS', 'lerp_color', 'parse_color', 'EffectDiscovery', 'RGBColor'] 
